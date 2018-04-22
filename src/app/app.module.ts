7b073f04160989571615dd15ba403bb0df76e008
obabichev <Aluckard.13>
2018-04-22 09:31:44 +0300

    transactions list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 
 import {AppComponent} from './app.component';
@@ -7,18 +8,42 @@ import {TransactionsListComponent} from './transactions-list/transactions-list.c
 import {TransactionCreateComponent} from './transaction-create/transaction-create.component';
 import {FormsModule} from '@angular/forms';
 import {DateValueAccessorModule} from 'angular-date-value-accessor';
+import {TransactionComponent} from './transaction/transaction.component';
+import {HttpClientModule} from '@angular/common/http';
 
+const appRoutes: Routes = [
+  {
+    path: 'transactions',
+    component: TransactionComponent,
+    data: {title: 'Transactions list'}
+  },
+  {
+    path: 'transaction-create',
+    component: TransactionCreateComponent
+  },
+  {
+    path: '',
+    redirectTo: '/transactions',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     TransactionsListComponent,
-    TransactionCreateComponent
+    TransactionCreateComponent,
+    TransactionComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    DateValueAccessorModule
+    DateValueAccessorModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/transaction/transaction.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-transaction',
+  templateUrl: './transaction.component.html',
+  styleUrls: ['./transaction.component.css']
+})
+export class TransactionComponent implements OnInit {
+
+  transactions: any;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get('/transaction').subscribe(data => {
+      this.transactions = data;
+    });
+  }
+
+}
