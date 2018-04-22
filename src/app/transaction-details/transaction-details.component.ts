1d9b41002326c312db8b77dccdbd5908b50e2f2e
obabichev <Aluckard.13>
2018-04-22 10:17:08 +0300

    transaction detailes

--- src/app/transaction-details/transaction-details.component.ts
+++ src/app/transaction-details/transaction-details.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ActivatedRoute} from '@angular/router';
+import {Transaction} from '../transaction';
 
 @Component({
   selector: 'app-transaction-details',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionDetailsComponent implements OnInit {
 
-  constructor() { }
+  transaction: Transaction;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
+  }
 
   ngOnInit() {
+    this.getTransactionDetail(this.route.snapshot.params['id']);
+  }
+
+  getTransactionDetail(id) {
+    this.http.get('/transaction/' + id).subscribe(data => {
+      this.transaction = data;
+    });
   }
 
 }
--- src/app/transaction/transaction.component.ts
+++ src/app/transaction/transaction.component.ts
@@ -18,5 +18,4 @@ export class TransactionComponent implements OnInit {
       this.transactions = data;
     });
   }
-
 }
